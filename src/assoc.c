00acedcd9df3947ebce845217180abaa5fedba8d
cdde <3018231-cdde@users.noreply.gitlab.com>
2021-01-14 20:52:52 -0600

    Adding work

--- src/assoc.c
+++ src/assoc.c
@@ -1,7 +1,15 @@
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct {
-	char** keys[];
-	char** values[];
+	char* key;
+	char* value;
+} __DgDict_node;
+
+typedef struct {
+	__DgDict_node* nodes;
 } DgDict;
 
+void DgAddToDict(char* k, char* v) {
+	
+}
---
+++ src/types.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <inttypes.h>
+
+typedef unsigned char bytef_t;
+typedef uint8_t byte_t;
+typedef uint8_t byte;
