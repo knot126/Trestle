fa8aac200b8957dd79ec6b5b12568152c3a717a4
cdde <3018231-cdde@users.noreply.gitlab.com>
2021-02-25 19:59:45 -0600

    Adding header declarations to math utils

--- src/util/maths.h
+++ src/util/maths.h
@@ -31,3 +31,31 @@ typedef struct DgVec4 {
 } DgVec4;
 
 typedef DgVec4 DgColour;
+
+// DgVec2
+
+DgVec2 DgVec2Add(DgVec2 a, DgVec2 b);
+DgVec2 DgVec2Subtract(DgVec2 a, DgVec2 b);
+DgVec2 DgVec2Multiply(DgVec2 a, DgVec2 b);
+float DgVec2Magnitude(DgVec2 a);
+float DgVec2Dot(DgVec2 a, DgVec2 b);
+DgVec2 DgVec2Normalise(DgVec2 a);
+
+// DgVec3
+
+DgVec3 DgVec3Add(DgVec3 a, DgVec3 b);
+DgVec3 DgVec3Subtract(DgVec3 a, DgVec3 b);
+DgVec3 DgVec3Multiply(DgVec3 a, DgVec3 b);
+float DgVec3Magnitude(DgVec3 a);
+float DgVec3Dot(DgVec3 a, DgVec3 b);
+DgVec3 DgVec3Cross(DgVec3 a, DgVec3 b);
+DgVec3 DgVec3Normalise(DgVec3 a);
+
+// DgVec4
+
+DgVec4 DgVec4Add(DgVec4 a, DgVec4 b);
+DgVec4 DgVec4Subtract(DgVec4 a, DgVec4 b);
+DgVec4 DgVec4Multiply(DgVec4 a, DgVec4 b);
+float DgVec4Magnitude(DgVec4 a);
+float DgVec4Dot(DgVec4 a, DgVec4 b);
+DgVec4 DgVec4Normalise(DgVec4 a);
