bd065006b84ef20d7211e5c4f31257eb3695721c
cdde <3018231-cdde@users.noreply.gitlab.com>
2021-03-06 17:57:30 -0600

    YAYYAYYAYAAYY!

--- src/graphics/opengl.c
+++ src/graphics/opengl.c
@@ -320,17 +320,6 @@ DgOpenGLContext* gl_graphics_init(void) {
 	
 	gl_error_check(__FILE__, __LINE__);
 	
-	// Simple matris transform
-	DgMat4 xform = DgMat4New(1.0f);
-	xform = DgMat4Rotate(xform, DgVec3New(0.0f, 0.0f, 1.0f), 0.25f);
-	xform = DgMat4Scale(xform, DgVec3New(0.5f, 0.5f, 0.5f));
-	xform = DgMat4Translate(xform, DgVec3New(0.0f, 0.5f, 0.0f));
-	DgMat4Print(xform);
-	
-	glUseProgram(gl->programs[0]);
-	glUniformMatrix4fv(glGetUniformLocation(gl->programs[0], "xform"), 1, GL_TRUE, &xform.ax);
-	glUseProgram(0);
-	
 	printf("Graphics subsystem has been initialised.\n");
 	
 	return gl;
@@ -372,7 +361,14 @@ void gl_graphics_update(DgOpenGLContext* gl) {
 	
 	glUseProgram(gl->programs[0]);
 	
-	glUniform1f(glGetUniformLocation(gl->programs[0], "mixValue"), mixValue);
+	//glUniform1f(glGetUniformLocation(gl->programs[0], "mixValue"), mixValue);
+	
+	// Simple matris transform
+	DgMat4 xform = DgMat4New(1.0f);
+	xform = DgMat4Rotate(xform, DgVec3New(0.0f, 0.0f, 1.0f), DgTime() * 0.25f);
+	xform = DgMat4Scale(xform, DgVec3New(0.5f, 0.5f, 0.5f));
+	xform = DgMat4Translate(xform, DgVec3New(0.0f, 0.5f, 0.0f));
+	glUniformMatrix4fv(glGetUniformLocation(gl->programs[0], "xform"), 1, GL_TRUE, &xform.ax);
 	
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, gl->textures[0]);
