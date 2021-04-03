c4c717422c78765bc4e9a8e2d5a9fb76d03edf49
cdde <3018231-cdde@users.noreply.gitlab.com>
2021-04-03 13:53:19 -0500

    Reorganise some things, make include-all header file

---
+++ src/util/util.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2021 Decent Games
+ * ===============================
+ * 
+ * NOTE: This is an auto-generated file. Please re-generate with "./.gen_util_h.py"
+ */
+
+#pragma once
+
+#include "bag.h"
+#include "maths.h"
+#include "rand.h"
+#include "strmap.h"
+#include "util.h"
+#include "thread.h"
+#include "flag.h"
+#include "fail.h"
+#include "time.h"
+#include "alloc.h"
+#include "dstring.h"
+#include "array.h"
+#include "xml.h"
