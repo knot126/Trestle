c6fa7cbfecfca2dd63b02f095624d5c7d4d8add0
cdde <3018231-cdde@users.noreply.gitlab.com>
2021-04-15 18:34:41 -0500

    Another Dictionary Imeplemntation

---
+++ src/util/dict.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2021 Decent Games
+ * ===============================
+ * 
+ * Dictionary - Hopefully the last implementation!
+ */
+
+#include <string.h>
+
+#include "alloc.h"
+
+#include "dict.h"
+
+DgDict DgDictNew(void) {
+	DgDict dict;
+	
+	memset(&dict, 0, sizeof(DgDict));
+	
+	return dict;
+}
+
+DgDict *DgDictNew2(void) {
+	DgDict *dict = (DgDict *) DgAlloc(sizeof(DgDict));
+	
+	if (dict) {
+		memset(dict, 0, sizeof(DgDict));
+	}
+	
+	return dict;
+}
+
+void DgDictSet(DgDict *dict, DgDictItem *key, DgDictItem *value) {
+	for (size_t i = 0; i < dict->length; i++) {
+		if (dict->data[i].key.type == key->type && dict->data[i].key.data.asPtr == key->data.asPtr) {
+			DgDictItem *k = &dict->data[i].key;
+			
+			if (k->type == DG_DICT_STRING || k->type == DG_DICT_ARRAY || k->type == DG_DICT_DICT) {
+				DgFree(k->data.asPtr);
+			}
+			
+			dict->data[i].value = *value;
+			
+			return;
+		}
+	}
+	
+	dict->data = (DgDictPair *) DgRealloc(dict->data, sizeof(DgDictPair) * ++dict->length);
+	dict->data[dict->length - 1].key = *key;
+	dict->data[dict->length - 1].value = *value;
+}
+
+DgDictItem* DgDictGet(DgDict *dict, DgDictItem *key) {
+	for (size_t i = 0; i < dict->length; i++) {
+		if (dict->data[i].key.type == key->type && dict->data[i].key.data.asPtr == key->data.asPtr) {
+			return &dict->data[i].value;
+		}
+	}
+}
+
