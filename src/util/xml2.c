e3d1e8623678868609074f789a0d4a09bcaecc8d
cdde <3018231-cdde@users.noreply.gitlab.com>
2021-04-25 19:01:24 -0500

    Restart with DGXML2

--- src/util/xml2.c
+++ src/util/xml2.c
@@ -33,11 +33,6 @@ typedef struct {
  * Utility Functions
  */
 
-#define ALLOC_ERROR_CHECK(x) if (!x) { \
-				printf("\033[1;33mWarning:\033[0m XML Parser Error: Failed to allocate memory for string.\n"); \
-				return 1; \
-			}
-
 static Dg_XMLParseToken *token_list_expand(Dg_XMLParseToken *data, size_t * const current_size) {
 	*current_size = *current_size + 1;
 	return (Dg_XMLParseToken *) DgRealloc(data, *current_size * sizeof(Dg_XMLParseToken));
@@ -99,132 +94,7 @@ uint32_t DgXML2Parse(DgXML2Node * const doc, const uint32_t content_size, const 
 	size_t token_count = 0;
 	
 	for (size_t i = 0; i < content_size; i++) {
-		printf("\n============ Char into iter: '%c' ============\n\n", content[i]);
-		
-		if (is_tag_start(&content[i])) {
-			tokens = token_list_expand(tokens, &token_count);
-			
-			// Handle </closing> tags
-			if (is_tag_start(&content[i]) == 2) {
-				printf("Found ending tag!\n");
-				i++;
-				size_t start = i;
-				
-				while (is_tag_end(&content[i]) != 1) {
-					i++;
-				}
-				
-				tokens[token_count - 1].type = DG_XML_TAG_END;
-				tokens[token_count - 1].text = DgStrdupl(&content[start], i - start);
-				
-				continue;
-			}
-			
-			printf("Found starting tag!\n");
-			
-			// Now we can handle normal tags
-			// Tag name start location
-			i++;
-			size_t start = i;
-			
-			while (!is_whitespace(&content[i]) && !is_tag_end(&content[i])) {
-				i++;
-			}
-			
-			char *name = DgStrdupl(&content[start], i - start);
-			
-			ALLOC_ERROR_CHECK(name);
-			
-			tokens[token_count - 1].type = DG_XML_TAG_START;
-			tokens[token_count - 1].text = name;
-			
-			printf("Found node with name: %s\n", tokens[token_count - 1].text);
-			
-			// Next chceck for attributes then ending of tag
-			while (true) {
-				printf("Key/Value Iteration!\n");
-				while (is_whitespace(&content[i])) {
-					i++;
-				};
-				
-				int endtype = is_tag_end(&content[i]);
-				
-				if (endtype == 2) {
-					tokens = token_list_expand(tokens, &token_count);
-					
-					tokens[token_count - 1].type = DG_XML_TAG_END;
-					tokens[token_count - 1].text = DgStrdup(name);
-				}
-				else if (endtype == 1) {
-					break;
-				}
-				
-				do {
-					i++;
-				} while (is_whitespace(&content[i]));
-				
-				// Otherwise we can add attribute
-				start = i;
-				
-				while (!is_assoc(&content[i])) {
-					i++;
-					
-					if (is_whitespace(&content[i])) {
-						// handle whitespace between assocaitor
-						printf("\033[1;33mWarning:\033[0m XML Parser Error: Found whitespace in assocaitor.\nRest of document:\n%s\n", &content[i]);
-						return 1;
-					}
-				}
-				
-				char * key = DgStrdupl(&content[start], i - start);
-				
-				ALLOC_ERROR_CHECK(key);
-				
-				// Add token for key
-				tokens = token_list_expand(tokens, &token_count);
-				tokens[token_count - 1].type = DG_XML_KEY;
-				tokens[token_count - 1].text = key;
-				
-				i++;
-				if (!is_string_term(&content[i])) {
-					// handle this case (ie. abc=def")
-					printf("\033[1;33mWarning:\033[0m XML Parser Error: Found key with malformed value (no quote after assocaitor), got %c instead.\n", content[i]);
-					return 1;
-				}
-				i++;
-				
-				// Store value
-				start = i;
-				
-				do {
-					i++;
-				} while (!is_string_term(&content[i]));
-				
-				char * value = DgStrdupl(&content[start], i - start);
-				
-				ALLOC_ERROR_CHECK(value);
-				
-				printf(" Got %s = %s.\n", key, value);
-				
-				// Add token for value
-				tokens = token_list_expand(tokens, &token_count);
-				tokens[token_count - 1].type = DG_XML_VALUE;
-				tokens[token_count - 1].text = value;
-			}
-		}
-		
-		if (is_comment_start(&content[i])) {
-			while (!is_comment_end(&content[i])) {
-				i++;
-			}
-			i += 3;
-		}
-	}
-	
-	printf("Have %d tokens.\n", token_count);
-	
-	for (size_t i = 0; i < token_count; i++) {
-		printf("\ttype=%.4d: text=%s\n", tokens[i].type, tokens[i].text);
+		printf("%c", content[i]);
 	}
 }
 
@@ -264,5 +134,3 @@ uint32_t DgXML2Load(DgXML2Node *doc, const char * const path) {
 	
 	return DgXML2Parse(doc, doc_size, content);
 }
-
-#undef ALLOC_ERROR_CHECK
