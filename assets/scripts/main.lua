4a02fdbaafb93642065c96876811a428b3bfd40c
cdde <3018231-cdde@users.noreply.gitlab.com>
2021-07-11 16:47:30 -0500

    Get set_camera working

--- assets/scripts/main.lua
+++ assets/scripts/main.lua
@@ -5,8 +5,17 @@
 -- Game Startup Script
 -- 
 
+ENT_TRANSFORM = (1 << 0)
+ENT_GRAPHICS_MESH = (1 << 1)
+ENT_GRAPHICS_CURVE = (1 << 2)
+
 function init()
+	e = create_entity(ENT_TRANSFORM)
 	
+	print("New entity name is ", e)
+	
+	push_transform(e, 1.0, 2.0, 3.0)
+	set_camera(e)
 end
 
 function tick()
--- src/game/scripting.c
+++ src/game/scripting.c
@@ -12,14 +12,86 @@
 #include "lualib.h"
 #include "lauxlib.h"
 
+#include "global/supervisor.h"
+#include "util/log.h"
 #include "util/script.h"
 #include "input/input.h"
 #include "game/box.h"
+#include "types.h"
 
 #include "scripting.h"
 
+static int scripted_EntityName(lua_State *script) {
+	if (lua_gettop(script) != 0) {
+		DgLog(DG_LOG_ERROR, "Invalid usage of next_name().");
+		return 0;
+	}
+	
+	lua_pushinteger(script, sup_next_name(supervisor(NULL)));
+	
+	return 1;
+}
+
+static int scripted_CreateEntity(lua_State *script) {
+	if (lua_gettop(script) != 1) {
+		DgLog(DG_LOG_ERROR, "Invalid usage of create_entity().");
+		return 0;
+	}
+	
+	uint64_t systems = lua_tointeger(script, 1);
+	
+	lua_pushinteger(script, sup_entity(supervisor(NULL), systems));
+	
+	return 1;
+}
+
+static int scripted_PushTransform(lua_State *script) {
+	int top = lua_gettop(script);
+	
+	if (top < 4) {
+		DgLog(DG_LOG_ERROR, "Invalid usage of push_transform().");
+		return 0;
+	}
+	
+	DgVec3 pos = {0.0f, 0.0f, 0.0f}, rot = {0.0f, 0.0f, 0.0f}, scale = {1.0f, 1.0f, 1.0f};
+	
+	Name name = lua_tointeger(script, 1);
+	
+	pos.x = lua_tointeger(script, 2);
+	pos.y = lua_tointeger(script, 3);
+	pos.z = lua_tointeger(script, 4);
+	
+	if (top > 6) {
+		rot.x = lua_tointeger(script, 5);
+		rot.y = lua_tointeger(script, 6);
+		rot.z = lua_tointeger(script, 7);
+	}
+	
+	if (top > 9) {
+		scale.x = lua_tointeger(script, 8);
+		scale.y = lua_tointeger(script, 9);
+		scale.z = lua_tointeger(script, 10);
+	}
+	
+	graph_set(&(supervisor(NULL)->graph), name, (Transform) {pos, rot, scale});
+	
+	return 0;
+}
+
+static int scripted_SetCamera(lua_State *script) {
+	if (lua_gettop(script) != 1) {
+		DgLog(DG_LOG_ERROR, "Invalid usage of set_camera().");
+		return 0;
+	}
+	
+	graphics_set_camera(&(supervisor(NULL)->graphics), lua_tointeger(script, 1));
+	
+	return 0;
+}
+
 static int scripted_GetKey(lua_State *script) {
 	if (lua_gettop(script) != 1) {
+		DgLog(DG_LOG_ERROR, "Invalid usage of get_key().");
 		return 0;
 	}
 	
@@ -31,5 +103,16 @@ static int scripted_GetKey(lua_State *script) {
 }
 
 void registerWorldScriptFunctions(DgScript *script) {
-	lua_register(script->state, "mgGetKey", &scripted_GetKey);
+	// Supervisor and Entites
+	lua_register(script->state, "next_name", &scripted_EntityName);
+	lua_register(script->state, "create_entity", &scripted_CreateEntity);
+	
+	// Scene Graph
+	lua_register(script->state, "push_transform", &scripted_PushTransform);
+	
+	// Graphics
+	lua_register(script->state, "set_camera", &scripted_SetCamera);
+	
+	// Input
+	lua_register(script->state, "get_key", &scripted_GetKey);
 }
