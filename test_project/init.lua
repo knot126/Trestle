dd36b2377fa0db518353ac85c901f93b4fc23671
knot126 <73200930+knot126@users.noreply.github.com>
2022-07-12 19:43:45 +0000

    More changes

--- test_project/init.lua
+++ test_project/init.lua
@@ -1,5 +1,8 @@
 function init()
-	
+	print("Init called!")
+	print("")
+	print("You are using the Trestle test project! If you mean to load a game, plese make")
+	print("sure you have used ./[exec name] --assets [path to folder]")
 end
 
 function tick()
@@ -7,5 +10,5 @@ function tick()
 end
 
 function free()
-	
+	print("Free called!")
 end
